1336abb1294313043035c10b2fe8671dfbabd6cd
tomas vaclavik <tomas@touchtribe.nl>
2015-04-17 17:12:37 +0200

    config file reading implemented.

--- src/OrientationSupport.cs
+++ src/OrientationSupport.cs
@@ -11,6 +11,9 @@ using Microsoft.Phone.Info;
 using WPCordovaClassLib.Cordova;
 using WPCordovaClassLib.Cordova.Commands;
 using WPCordovaClassLib.Cordova.JSON;
+using System.Windows.Resources;
+using System.IO;
+using System.Xml.Linq;
 
 namespace Cordova.Extension.Commands
 {
@@ -18,21 +21,50 @@ namespace Cordova.Extension.Commands
     /// </summary>
     public class OrientationSupport: BaseCommand
     {
+        private int prefDelay = 12345; 
+
+
+        public OrientationSupport()
+        {
+            LoadConfigPrefs();
+
+            System.Diagnostics.Debug.WriteLine(">>>>>>>>>>>>>>inited from plugin...value: "+ this.prefDelay);
+        }
+
+
+        private void LoadConfigPrefs()
+        {
+            StreamResourceInfo streamInfo = Application.GetResourceStream(new Uri("config.xml", UriKind.Relative));
+            if (streamInfo != null)
+            {
+                using (StreamReader sr = new StreamReader(streamInfo.Stream))
+                {
+                    //This will Read Keys Collection for the xml file
+                    XDocument configFile = XDocument.Parse(sr.ReadToEnd());
+                    
+                    string configDelay = configFile.Descendants()
+                                      .Where(x => (string)x.Attribute("name") == "SplashScreenDelay")
+                                      .Select(x => (string)x.Attribute("value"))
+                                      .FirstOrDefault();
+                    int nVal;
+                    prefDelay = int.TryParse(configDelay, out nVal) ? nVal : prefDelay;
+                }
+            }
+        }
+
         /// <summary>
         /// </summary>
         public void applyOrientationSettings(string options)
         {
-            System.Diagnostics.Debug.WriteLine("options for plugin: " + options);
+            System.Diagnostics.Debug.WriteLine(">>>>>>>>>>>options for plugin: " + options);
 
             var resolution = (Size)DeviceExtendedProperties.GetValue("PhysicalScreenResolution");
             var width = resolution.Width.ToString();
             var height = resolution.Height.ToString();
             var result = "{\"width\":\"" + width + "\",\"height\":\"" + height + "\"}";
 
-            System.Diagnostics.Debug.WriteLine("result from plugin: " + result);
+            System.Diagnostics.Debug.WriteLine(">>>>>>>>>>result from plugin: " + result);
             //DispatchCommandResult(new PluginResult(PluginResult.Status.OK, result));
         }
     }
 }
-
-
